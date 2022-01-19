d737b339154b91b9b51c750e99ae10f4855e9ad2
Phelipe Sousa <71620144+phelipe06@users.noreply.github.com>
2022-01-18 21:30:14 -0300

    Create teste.sh

---
+++ teste.sh
@@ -0,0 +1,32 @@
+#!/bin/bash;
+      #phelipe.sete
+      #teste
+
+
+echo
+  echo
+    echo
+      echo
+clear
+screenfetch
+  setterm --foreground red
+  toilet TESTE
+      echo
+    echo
+  echo
+  setterm --foreground cyan
+echo "             ____________________________           "
+setterm --foreground cyan
+echo "             |     𝙲𝚁𝙸𝙰𝙳𝙾𝚁  𝙵𝙴𝙻𝙸𝙿𝙴      |           "
+echo "             |     𝚅𝙴𝚁𝚂𝙰𝙾 3.0           |           "
+echo "             |__________________________|           "
+setterm --foreground red
+echo "                   R.D.P 𝚂𝙴𝙲𝚄𝚁𝙸𝚃𝚈                   "
+setterm --foreground cyan
+echo "             |__________________________|           "
+echo
+echo
+    setterm  --forengraud "green"
+  echo " Loading..."
+apt-get update > install.log
+python geracc.txt
